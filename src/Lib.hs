3764fd5e94008d9a8e12a6fc654faa7becfb8290
dclxvi13 <dclxvi.13.x@gmail.com>
2018-04-14 12:58:54 +0300

    base logic

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,54 @@
-module Lib
-    ( someFunc
-    ) where
+module Lib where
 
-someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+newtype TState = TState String
+newtype TCondition = TCondition String
+newtype TAction = TAction String
+
+data TCase = TCase {
+  _caseState      :: TState,
+  _caseConditions :: [TCondition],
+  _caseAction     :: TAction
+}
+
+----------------------------------------
+-- Get globals
+----------------------------------------
+
+getGlobalActions :: [TAction]
+getGlobalActions = []
+
+getGlobalConditions :: [TCondition]
+getGlobalConditions = []
+
+getStates :: [TState]
+getStates = []
+
+----------------------------------------
+-- Get locals
+----------------------------------------
+
+getActionsForState :: TState -> [TAction]
+getActionsForState state = []
+
+getConditionsForState :: TState -> [TCondition]
+getConditionsForState state = []
+
+----------------------------------------
+-- Cases
+----------------------------------------
+
+buildCasesForState :: TState -> [TCondition] -> [TAction] -> [TCondition] -> [TAction] -> [TCase]
+buildCasesForState state globalConditions globalActions stateConditions stateActions =
+  let conds =[[]] ++
+             [[c] | c <- globalConditions ++ stateConditions] ++
+             [[g, s] | g <- globalConditions, s <- stateConditions]
+  in
+  [TCase state cond action | cond <- conds, action <- globalActions ++ stateActions]
+
+getCases :: [TCase]
+getCases =
+  let states = getStates
+      globalActions = getGlobalActions
+      globalConditions = getGlobalConditions
+  in
+  concatMap (\state -> buildCasesForState state globalConditions globalActions (getConditionsForState state) (getActionsForState state)) states
